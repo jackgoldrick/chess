6f5004eef57040b5e72308095ff8e0becaa61117
jackgoldrick <jago5326@colorado.edu>
2024-01-31 15:08:35 -0700

    worked on bishop travel logic in n

---
+++ src/bishop.cpp
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "piece.h"
+#include "bishop.h"
+
+bool bishop::check_path(int location, board* brd){
+    int piece_loc = this->get_location();
+    int row = get_row(piece_loc);
+    int col_diff = abs(piece_loc - location);
+
+   // int bound = get_row(piece_loc);
+    int direction = 1;
+    int loop_size = 8;
+   
+      
+  
+
+
+    /** this only handles "behind", left diagnol not implemented since it is ahead on a number line
+     *  the difference in row or locations should tell us the amount of iterations needed
+     *  then i can add 9 or 7 to move diagnolly 
+     *  add 7 should move up and left and 9 should move up and right
+     *  the subtraaction case should follow a similar basis 
+     */
+    if (location < piece_loc) { 
+        direction = -1;
+        loop_size = 0;
+
+    }
+
+    for (int i = 1; i < abs(loop_size - row); i++ ) {
+
+        if (!brd->is_empty(piece_loc + (direction * 8 * i))) return false;
+        
+    }
+
+
+
+}
